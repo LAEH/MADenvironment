8eca3f83f9460fc19fef3dd02708a322aba3a697
MAD <la@madbits.com>
2014-07-13 01:13:26 -0400

    Better

--- coloring.lua
+++ coloring.lua
@@ -16,40 +16,47 @@ function coloring.general()
    return table.concat(scopes)
 end
 
-
+local languages = {'lua','css','markdown'}
 function coloring.languages()
-   local scopes = {}
-   for _,scope in ipairs(scheme.language.lua) do
-      local scopeStrings = {}
-      local name = scope.name
-      local color = scope.color
-      local selector = scope.selector
-      local style, back = ' ', ' '
-      if scope.style then style = scope.style end
-      if scope.back  then back = scope.back end
-      table.insert(scopeStrings,
-         [[ <dict>
-               <key>name</key>
-               <string>]]..name..[[</string>
-               <key>scope</key>
-               <string>]]..selector..[[</string>
-               <key>settings</key>
-                  <dict>
-                     <key>foreground</key>
-                     <string>]]..color..[[</string>
-                     <key>fontStyle</key>
-                     <string>]]..style..[[</string>
-                     <key>background</key>
-                     <string>]]..back..[[</string>
+   local allScopesStrings = {}
+   -- print(scheme.language)
+   for _,lang in ipairs(languages) do
+      local syntaxTheme = scheme.language[lang]
+      local langScopesStrings = {}
+      for _,scope in ipairs(syntaxTheme) do
+         print(scope.color)
+         local scopeStrings = {}
+         local name = scope.name
+         local color = scope.color
+         local selector = scope.selector
+         local style, back = ' ', ' '
+         if scope.style then style = scope.style end
+         if scope.back  then back = scope.back end
+         table.insert(scopeStrings,
+            [[ <dict>
+                  <key>name</key>
+                  <string>]]..name..[[</string>
+                  <key>scope</key>
+                  <string>]]..selector..[[</string>
+                  <key>settings</key>
+                     <dict>
+                        <key>foreground</key>
+                        <string>]]..color..[[</string>
+                        <key>fontStyle</key>
+                        <string>]]..style..[[</string>
+                        <key>background</key>
+                        <string>]]..back..[[</string>
+                  </dict>
                </dict>
-            </dict>
-         ]]
-      )
-      table.insert(scopes, table.concat(scopeStrings))
+            ]]
+         )
+         table.insert(langScopesStrings, table.concat(scopeStrings))
+      end
+      table.insert(allScopesStrings, table.concat(langScopesStrings))
    end
-   return table.concat(scopes)
+   return table.concat(allScopesStrings)
 end
-
+coloring.languages()
 function coloring.xmlMake()
    local xmlDoc = [[
       <?xml version="1.0" encoding="UTF-8"?>
@@ -85,12 +92,16 @@ function coloring.xmlMake()
       </plist>
    ]]
    print(col.blue(xmlDoc))
-   local schemeDirectory = "/Users/LA/Library/Application Support/Sublime Text 3/Packages/MAD/"
-   local schemeName = 'MAD13.tmTheme'
-   local schemeFile = schemeDirectory..'/'..schemeName
-   local file = io.open(schemeFile,'w')
+   local schemeDirectory = os.getenv('HOME')..'/Library/Application Support/Sublime Text 3/Packages/MAD/'
+   local schemeName = 'MAD01.tmTheme'
+   local sublimeFile = schemeDirectory..'/'..schemeName
+   -- Sublime Package Version
+   local file = io.open(sublimeFile,'w')
    file:write(xmlDoc)
    file:close()
+   -- Repo Version
+   local fileRepo = io.open(schemeName,'w')
+   fileRepo:write(xmlDoc)
+   fileRepo:close()
 end
-
 coloring.xmlMake()
--- schemes.lua
+++ schemes.lua
@@ -1,29 +1,18 @@
 #!/usr/bin/env th
 
---┏━━━━━━━━━━━━━━┓
---┃              ┃
---┃              ┃
---┃              ┃
---┃              ┃
---┗━━━━━━━━━━━━━━┛
-
 -- COLORS
-local base = '#eeeeee'
-local colors = {
-   violet       = { front='#530ff5', back ='#e3f9fd'},
-   cyan         = { front='#2cbae7', back ='#cbebf5'},
-   green        = { front='#65b11a', back ='#f0f7ee'},
-   pink         = { front='#fe2973', back ='#fcf1f4'},
-   orange       = { front='#ffa502', back ='#fff6e6'},
-   red          = { front='#ff5533', back ='#ffeeeb'},
-   yellow       = { front='#ffd201', back ='#fffbe6'},
-   grey         = { front='#9fa0a4', back ='#f6f6f6'},
-   black        = { front='#9fa0a4', back ='#f6f6f6'},
-   lightblue    = { front='#65d0fc', back ='#f0fbff'},
-   blue         = { front='#2f87fb', back ='#eaf3ff'},
-}
-
--- SCHE TABLE BUILDING
+local base         = '#eeeeee'
+local violet       = { front='#530ff5', back ='#e3f9fd'}
+local cyan         = { front='#2cbae7', back ='#cbebf5'}
+local green        = { front='#65b11a', back ='#f0f7ee'}
+local pink         = { front='#fe2973', back ='#fcf1f4'}
+local orange       = { front='#ffa502', back ='#fff6e6'}
+local red          = { front='#ff5533', back ='#ffeeeb'}
+local yellow       = { front='#ffd201', back ='#fffbe6'}
+local grey         = { front='#9fa0a4', back ='#f6f6f6'}
+local black        = { front='#9fa0a4', back ='#f6f6f6'}
+local lightblue    = { front='#65d0fc', back ='#f0fbff'}
+local blue         = { front='#2f87fb', back ='#eaf3ff'}
 
 scheme = {
    general = {},
@@ -34,7 +23,7 @@ scheme.general = {
    { selector = 'background',        color =  base},
    { selector = 'invisibles',        color = '#E0E0E0'},
    { selector = 'lineHighlight',     color = '#ffffff'},
-   { selector = 'gutterForeground',  color = '#bbbee1'},
+   { selector = 'gutterForeground',  color = '#bbbbbb'},
    { selector = 'foreground',        color = '#222222'},
    { selector = 'selection',         color = '#ffffff'},
    { selector = 'selectionBorder',   color = '#000000'},
@@ -47,86 +36,88 @@ scheme.general = {
 }
 
 scheme.language.lua = {
-   -- QUOTES
+
+   --QUOTES
    {
       selector = 'string.quoted.single',
       name = 'LUA block string', -- [[string]]
-      color = colors.pink.front,
-      back = colors.pink.back,
+      color = pink.front,
+      back = pink.back,
       style='bold italic',
    },
    {
       selector = 'string.quoted.double',
-      name     = 'LUA string',
-      color    ='#000000',
-      back     = base,
-      style    ='bold italic',
+      name = 'LUA string',
+      color ='#000000',
+      back = base,
+      style ='bold italic',
    },
    {
       selector= 'constant.character, string',
       name = 'LUA block quote',
-      color = colors.green.front,
+      color = green.front,
       style = 'bold',
    },
-   -- COMMENTS
+
+   --COMMENTS
    {
-      name = 'LUA comment', -- this is a comment
+      name = 'LUA comment',
       color = '#000',
       style='bold italic',
       selector = 'comment, comment punctuation',
    },
    {
-      name = 'LUA comments block', -- [[string]]
-      color = colors.pink.front,
+      name = 'LUA comments block',
+      color = pink.front,
       style='bold italic',
       selector = 'comment.block',
    },
-   --LANGUAGE KEYWORDS & CONSTANTS
+
+   --KEYWORDS & CONSTANTS
    {
       name  = 'LUA support functions',-- 'io. table. os. math. .file'
       style = 'italic',
-      color = colors.blue.front,
+      color = blue.front,
       selector = 'support.function',
    },
    {
       name = 'LUA controls keyword', -- 'local' 'function' 'for' 'if'
-      back = colors.orange.back,
-      color = colors.orange.front,
+      back = orange.back,
+      color = orange.front,
       style = 'bold',
       selector = 'keyword.control',
    },
    {
       name = 'LUA arythmetic signs',--'==, =, >'
-      back = colors.violet.back,
-      color = colors.violet.front,
+      back = violet.back,
+      color = violet.front,
       selector  = 'keyword.operator',
    },
    {
       name = 'LUA constants',--'true, false'
-      color = colors.red.front,
+      color = red.front,
       style = 'bold',
       selector = 'constant.language',
    },
-   --┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-   --┃                  USER DEFINED ┃
-   --┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+   --USER
    {
-      name = 'LUA user function name',-- 'MADweb.' 'MADcolors. '
+      name = 'LUA user function name',-- 'MADweb.' 'MAD '
       style = 'italic bold',
-      color = colors.violet.front,
+      color = violet.front,
       selector = 'entity.name.function',
    },
    {
    {
       name = 'LUA user function arguments',--'opt'
-      color = colors.green.front,
-      back = colors.green.back,
+      color = green.front,
+      back = green.back,
       selector  = 'variable.parameter',
    },
       name = 'LUA numbers',--'2,3,4'
       color = '#000000',
       style = 'bold',
-      color = colors.red.front,
+      color = red.front,
       selector = 'constant',
    },
    {
@@ -136,124 +127,135 @@ scheme.language.lua = {
    },
    {
       name = 'LUA user strings',--strings, key strings, quotes'
-      color='#444',
-      color = colors.red.front,
+      style='bold',
+      color = '#000',
       selector= 'punctuation',
    }
 }
 
--- scheme.language.css = {
---    {
---       name = 'CSS Selector - Tag',
---       selector = 'entity.name.tag.css',
---       back = colors.pink.back,
---       color = colors.pink.front,
---       style = 'bold',
---    },
---    {
---       name = 'CSS Selector - Id',
---       selector = 'source.css entity.other.attribute-name.id'
---       back = colors.pink.back,
---       color = colors.pink.front,
---       style = 'bold',
---    },
---    {
---       name = 'CSS Selector - Class',
---       selector = 'entity.other.attribute-name.class'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Pseudo Class',
---       selector = 'entity.other.attribute-name.pseudo-element, entity.other.attribute-name.pseudo-class'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Property Name',
---       selector = 'support.type.property-name'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Property Value',
---       selector = 'support.constant.property-value'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Color',
---       selector = 'constant.other.color.rgb-value'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Unit',
---       selector = 'keyword.other.unit.css'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Font Name',
---       selector = 'support.constant.font-name'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Curly Brackets',
---       selector = 'punctuation.section.property-list.css'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Round Brackets',
---       selector = 'punctuation.section.function.css'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS url() / rgba() / hsla',
---       selector = 'support.function.misc.css'
---       back = colors.pink.back,
---       color = colors.pink.front,
---    },
---    {
---       name = 'CSS Parameter',
---       selector = 'variable.parameter.misc.css',
---       back = colors.pink.back,
---       color = colors.pink.front,
---    }
--- }
+scheme.language.css = {
+   {
+      selector = 'entity.name.tag.css',
+      name = 'CSS Selector - Tag',
+      back = pink.back,
+      color = pink.front,
+      style = 'bold',
+   },
+   {
+      selector = 'source.css entity.other.attribute-name.id',
+      name = 'CSS Selector - Id',
+      back = pink.back,
+      color = pink.front,
+      style = 'bold',
+   },
+   {
+      selector = 'entity.other.attribute-name.class',
+      name = 'CSS Selector - Class',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'entity.other.attribute-name.pseudo-element, entity.other',
+      name = 'CSS Pseudo Class,attribute-name.pseudo-class',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'support.type.property-name',
+      name = 'CSS Property Name',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'support.constant.property-value',
+      name = 'CSS Property Value',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'constant.other.color.rgb-value',
+      name = 'CSS Color',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'keyword.other.unit.css',
+      name = 'CSS Unit',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'support.constant.font-name',
+      name = 'CSS Font Name',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'punctuation.section.property-list.css',
+      name = 'CSS Curly Brackets',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'punctuation.section.function.css',
+      name = 'CSS Round Brackets',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'support.function.misc.css',
+      name = 'CSS url() / rgba() / hsla',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      selector = 'variable.parameter.misc.css',
+      name = 'CSS Parameter',
+      back = pink.back,
+      color = pink.front,
+   }
+}
 
--- scheme.language.markdown = {
---    {
---       name = 'Markdown punctuation',
---       selector = 'markup.list, text.html.markdown punctuation.definition, meta.separator.markdown',
---    },
---    {
---       name = 'Markdown heading',
---       selector = 'markup.heading',
---    },
---    {
---       name = 'Markdown text inside some block element',
---       selector = 'markup.quote, meta.paragraph.list',
---    },
---    {
---       name = 'Markdown em',
---       selector = 'markup.italic',
---    },
---    {
---       name = 'Markdown strong',
---       selector = 'markup.bold',
---    },
---    {
---       name = 'Markdown reference',
---       selector = 'markup.underline.link.markdown, meta.link.inline punctuation.definition.metadata, meta.link.reference.markdowdefinition.constant, meta.link.reference.markdown constant.other.reference',
---    },
---    {
---       name = 'Markdown linebreak',
---       selector = 'meta.paragraph.markdown meta.dummy.line-break',
---    }
--- }
+scheme.language.markdown = {
+   {
+      name = 'Markdown punctuation',
+      selector = 'markup.list, text.html.markdown punctuation.definition, meta.separator.markdown',
+      back = pink.back,
+      color = pink.front,
+   },
+   {
+      name = 'Markdown heading',
+      selector = 'markup.heading',
+      back = blue.back,
+      color = blue.front,
+   },
+   {
+      name = 'Markdown text inside some block element',
+      selector = 'markup.quote, meta.paragraph.list',
+      back = violet.back,
+      color = violet.front,
+   },
+   {
+      name = 'Markdown em',
+      selector = 'markup.italic',
+      back = red.back,
+      color = red.front,
+   },
+   {
+      name = 'Markdown strong',
+      selector = 'markup.bold',
+      color = red.front,
+   },
+   {
+      name = 'Markdown reference',
+      selector = 'markup.underline.link.markdown, meta.link.inline punctuation.definition.metadata, meta.link.reference.markdowdefinition.constant, meta.link.reference.markdown constant.other.reference',
+      color = red.front,
+   },
+   {
+      name = 'Markdown linebreak',
+      selector = 'meta.paragraph.markdown meta.dummy.line-break',
+      color = red.front,
+   }
+}
 
 return scheme
